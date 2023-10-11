6c183ac9b8acbb8ec3257b325f86641647680d54
Shaykaden <79753485+Shaykaden@users.noreply.github.com>
2023-10-11 15:52:08 +0200

    Update script description

--- tabify.js
+++ tabify.js
@@ -2,7 +2,7 @@
 // @name         AMQ Tab Selection
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  try to take over the world!
+// @description  Use the tab key to navigate through the anime selection.
 // @author       Shaykaden
 // @match        https://animemusicquiz.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=animemusicquiz.com
